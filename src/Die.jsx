14a6edb723c1842939bcfdcbcf455689ae012fed
Kat Grace <katherine.grace.reed@gmail.com>
2024-10-14 21:14:14 +0100

    Add conditional styles

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,6 +1,9 @@
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#ff00ff" : "#663399"
+    }
     return (
-        <article className="die-face">
+        <article className="die-face" style={styles}>
             <h2>{props.value}</h2>
         </article>
     )
